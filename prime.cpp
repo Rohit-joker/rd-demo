b0a6e9e44ece9462c023bdbd1144865f2f3f6577
Rohit-joker <rohitdhotre.sae.comp@gmail.com>
2026-02-14 15:27:56 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,84 @@
+ #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     bool a=true;
+//     cin>>num;
+
+//     for(int i=2;i<=num-1;i++){
+//          if(num%i==0){
+//             a=false;
+//             break;
+//          }
+//     }
+//    if (a==true)
+//    {
+//       cout<<num<<" it is prime"<<endl;
+//    }
+//    else{
+//       cout<<num<<" it is not prime"<<endl;
+//    }
+//    return 0;
+// }
+#include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+//     int fact=1;
+//     cin>>num;
+//     for(int i=1;i<=num;i++){
+//         fact=fact*i;
+//     }
+//     cout<<fact<<" it is factorial num"<<endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch;
+//     cin>>ch;
+//     if(ch>='a'&&ch<='z'){
+//         cout<<ch<<" it is lowwer case";
+//     }
+//     else if (ch>='A'&&ch<='Z')
+//     {
+//         cout<<ch<<" it is upper case";
+//     }
+//     else{
+//         cout<<ch<<" it isn't character";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+#include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+//     int fib=0;
+//     cout<<"how many numbers you want :";
+//     cin>>num;
+//     for (int i = 0; i <= num; i++)
+//     {
+//         fib=fib+i;
+//         cout<<fib<<"\t";
+//     }
+//     cout<<" \n it is fibonacci series"<<endl;
+//     return 0;
+    
+// }
+#include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+//     int sum=0;
+//     cout<<sum<<"\nenter last num:";
+//     cin>>num;
+//     for(int i=0;i<=num;i++){
+//         if(i%2!=0){
+//             sum=sum+i;
+//         }
+//     }
+//     cout<<sum<<endl;
+//     return 0;
+// }
